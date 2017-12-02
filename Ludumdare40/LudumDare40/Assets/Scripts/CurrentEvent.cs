626e980f9f5747fadb6c6750095f7a02b27e50df
Nguyen Duong <ducnguyenduchao@gmail.com>
2017-12-02 11:23:36 -0800

    Added simple json file reader - convert news format to an readable object

---
+++ Ludumdare40/LudumDare40/Assets/Scripts/CurrentEvent.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CurrentEvent : MonoBehaviour {
+
+    public TextAsset text;
+    public CEvents Events { get { return cEvents; } }
+
+    private CEvents cEvents;
+    private string path = "Assets/Resources/News/";
+    private string fileJson = "news1.json";
+    private string fileText = "news1.text";
+
+    void Start () {
+        ReadJson();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /**
+     * Read and deserialize a json file
+     **/
+    public void ReadJson()
+    {
+        StreamReader reader = new StreamReader(path + fileJson);
+        string jsonString = reader.ReadToEnd();
+        cEvents = JsonConvert.DeserializeObject<CEvents>(jsonString);
+        Debug.Log(cEvents.Conservative);
+    }
+
+
+    [Serializable]
+    public class Conservative
+    {
+        public List<string> GoodTweets { get; set; }
+        public List<string> BadTweets { get; set; }
+    }
+    [Serializable]
+    public class Liberal
+    {
+        public List<string> GoodTweets { get; set; }
+        public List<string> BadTweets { get; set; }
+    }
+    [Serializable]
+    public class CEvents
+    {
+        public string Event { get; set; }
+        public string Laws { get; set; }
+        public string Approve { get; set; }
+        public List<string> ApproveTweet { get; set; }
+        public string Veto { get; set; }
+        public List<string> VetoTweet { get; set; }
+        public Conservative Conservative { get; set; }
+        public Liberal Liberal { get; set; }
+    }
+}
---
+++ Ludumdare40/LudumDare40/Assets/Scripts/TV.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator DisplayTV(string text, float time)
+    {
+        float timeElapsed = 0;
+        while (timeElapsed >= time)
+        {
+            timeElapsed += Time.deltaTime;
+            yield return 0;
+        }
+    }
+}
