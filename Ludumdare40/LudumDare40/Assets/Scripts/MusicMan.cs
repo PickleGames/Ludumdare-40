5fea1dce233a0bcf3897b18b9fd27376fcd6a160
Miguel Garnica <mgarnica123@aol.com>
2017-12-04 11:23:48 -0800

    MusicPlayer (plays random music and switches to another random after certain amount of loops)

---
+++ Ludumdare40/LudumDare40/Assets/Scripts/MusicMan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMan : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip[] audioClips;
+    private AudioClip currentClip;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        PickRandomClip();
+        audioSource.Play();
+	}
+
+    float timesPlayed = 1;
+	// Update is called once per frame
+	void Update () {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+            timesPlayed++;
+        }
+
+        if (timesPlayed > 2)
+        {
+            PickRandomClip();
+            timesPlayed = 0;
+        }
+	}
+
+    public void PickRandomClip()
+    {
+        currentClip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.clip = currentClip;
+    }
+}
